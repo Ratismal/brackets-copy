5932a10f88c2c6860084dfe361b796b1632c3d33
ratismal <kirkbenm@gmail.com>
2015-10-15 12:37:23 -0600

    init

---
+++ script.js
@@ -0,0 +1,16 @@
+var lines = "<br>";
+
+for (i = 1; i < 50; i++) {
+        lines += i + "<br>";
+}
+
+function doIt()
+{
+document.getElementById("linecount").innerHTML = lines;
+}
+
+function format()
+{
+    var content = document.getElementById("content").innerHTML;
+    
+}
